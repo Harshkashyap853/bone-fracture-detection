cad5fdbd79405de1fcc28cdcf890f74f50b2f448
Harshkashyap853 <105189372+Harshkashyap853@users.noreply.github.com>
2023-12-17 00:49:08 +0530

    Update training_fracture.py

--- training_fracture.py
+++ training_fracture.py
@@ -8,6 +8,40 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications import MobileNetV3Large
 
 
+def load_path(path, part):
+    """
+    load X-ray dataset
+    """
+    dataset = []
+    for folder in os.listdir(path):
+        folder = path + '/' + str(folder)
+        if os.path.isdir(folder):
+            for body in os.listdir(folder):
+                if body == part:
+                    body_part = body
+                    path_p = folder + '/' + str(body)
+                    for id_p in os.listdir(path_p):
+                        patient_id = id_p
+                        path_id = path_p + '/' + str(id_p)
+                        for lab in os.listdir(path_id):
+                            if lab.split('_')[-1] == 'positive':
+                                label = 'fractured'
+                            elif lab.split('_')[-1] == 'negative':
+                                label = 'normal'
+                            path_l = path_id + '/' + str(lab)
+                            for img in os.listdir(path_l):
+                                img_path = path_l + '/' + str(img)
+                                dataset.append(
+                                    {
+                                        'body_part': body_part,
+                                        'patient_id': patient_id,
+                                        'label': label,
+                                        'image_path': img_path
+                                    }
+                                )
+    return dataset
+
+
 
 def create_model():
     pretrained_model = MobileNetV3Large(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling=None)
