bfbb02be9bca07ba20be121af4a5f1abbddff3ad
Harshkashyap853 <105189372+Harshkashyap853@users.noreply.github.com>
2023-12-17 17:47:24 +0530

    Update training_fracture.py

--- training_fracture.py
+++ training_fracture.py
@@ -70,9 +70,7 @@ def create_model():
 
     x = pretrained_model.output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dropout(0.5)(x)
     x = tf.keras.layers.Dense(512, activation='relu')(x)
-    x = tf.keras.layers.Dropout(0.5)(x)
     x = tf.keras.layers.Dense(256, activation='relu')(x)
    
     x = tf.keras.layers.Dense(128, activation='relu')(x)
